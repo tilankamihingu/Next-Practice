56f6ad7f8b24695d890d1ce9326386042fb71df2
Tilanka_Mihingu <tilankamihingu2018@gmail.com>
2021-05-30 08:12:26 +0530

    initial commit

---
+++ components/Nav.js
@@ -0,0 +1,19 @@
+import NavStyles from '../styles/Nav.module.css';
+import Link from 'next/link';
+
+const Nav = () => {
+    return (
+        <nav className={NavStyles.nav}>
+           <ul>
+               <li>
+                    <Link href='/'>Home</Link>
+               </li>
+               <li>
+                    <Link href='/about'>About</Link>
+               </li>
+           </ul>
+        </nav>
+    )
+}
+
+export default Nav
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function about() {
+    return (
+        <div>
+            <Head>
+                <title>Web dev news</title>
+                <meta name='keywords' content='web development programming' />
+            </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
